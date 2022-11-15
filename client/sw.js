667b1d0a24f94301b62bea9e676afa9ea6cf4c27
Julie J <jeannet.julie@gmail.com>
2022-11-15 18:03:02 +0100

    Ajout manifest.json, test installation et version offline des fichiers mis en cache

--- client/sw.js
+++ client/sw.js
@@ -1,7 +1,7 @@
-// const BASE = location.protocol + "//" + location.host;
+const BASE = location.protocol + "//" + location.host;
 const PREFIX = "V3";
 const CACHED_FILES = [
-  `/main.js`,`/style.css`,
+  `${BASE}/main.js`,`${BASE}/style.css`, `${BASE}/index.html`
 ];
 
 
@@ -10,7 +10,7 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     (async () => {
       const cache = await caches.open(PREFIX);
-      await cache.addAll(CACHED_FILES);
+      await cache.addAll([...CACHED_FILES, "/offline.html"]);
     })()
   );
   console.log(`${PREFIX} Install`);
@@ -29,3 +29,31 @@ self.addEventListener('push', event => {
     body: 'OK. Cela fonctionne ! ',
   });
 });
+
+self.addEventListener('fetch', event=>{
+  // bonne pratique : 
+  console.log(
+    `${PREFIX} Fetching : ${event.request.url}, Mode : ${event.request.mode}`
+  );
+  
+  if (event.request.mode === "navigate") {
+  event.respondWith(
+    (async () => {
+      try {
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
+        }
+
+        return await fetch(event.request);
+      } catch (e) {
+        const cache = await caches.open(PREFIX);
+        return await cache.match("/offline.html");
+      }
+    })()
+  )
+} else if (CACHED_FILES.includes(event.request.url)) {
+  event.respondWith(caches.match(event.request))}
+
+})
+
