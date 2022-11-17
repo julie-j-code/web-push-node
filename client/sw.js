27a3b7c4121163def1133810e29ac5faa1a8a58a
Julie J <jeannet.julie@gmail.com>
2022-11-17 07:07:52 +0100

    Ajout handler on activate sw et ajustements pour rapport Lighthouse

--- client/sw.js
+++ client/sw.js
@@ -1,7 +1,7 @@
 const BASE = location.protocol + "//" + location.host;
-const PREFIX = "V3";
+const PREFIX = "V4";
 const CACHED_FILES = [
-  `${BASE}/main.js`,`${BASE}/style.css`, `${BASE}/index.html`
+  `${BASE}/main2.js`,`${BASE}/style.css`, `${BASE}/index.html`, `${BASE}/img/node.png`
 ];
 
 
@@ -21,6 +21,42 @@ self.addEventListener("activate", () => {
   console.log(PREFIX)
 
 })
+
+// self.addEventListener("install", (installEvt) => {
+//   installEvt.waitUntil(
+//     caches
+//       .open(PREFIX)
+//       .then((cache) => {
+//         cache.addAll(CACHED_FILES);
+//       })
+//       .then(self.skipWaiting())
+//       .catch((e) => {
+//         console.log(e);
+//       })
+//   );
+// });
+
+
+
+//  la méthode waitUntil sur le service worker attend la fin de l'action, puis vérifie si les actifs que nous essayons d'effacer sont les actifs de notre application actuelle avant de les supprimer.
+
+self.addEventListener("activate", (event) => {
+  clients.claim();
+  event.waitUntil(
+    (async () => {
+      const keys = await caches.keys();
+      await Promise.all(
+        keys.map((key) => {
+          if (!key.includes(PREFIX)) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })()
+  );
+  console.log(`${PREFIX} Active`);
+});
+
 
 
 self.addEventListener('push', event => {
@@ -54,6 +90,14 @@ self.addEventListener('fetch', event=>{
   )
 } else if (CACHED_FILES.includes(event.request.url)) {
   event.respondWith(caches.match(event.request))} 
+
+// event.respondWith(
+//   fetch(event.request).catch(() => {
+//     return caches.open(PREFIX).then((cache) => {
+//       return cache.match(event.request);
+//     });
+//   })
+// );
 
 })
 
